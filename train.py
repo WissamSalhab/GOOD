947106e48a46ba061a3798fba29bb86a2f7ba17d
WissamSalhab <61253051+WissamSalhab@users.noreply.github.com>
2024-03-04 16:07:02 -0500

    Add files via upload

---
+++ train.py
@@ -0,0 +1,196 @@
+## Standard libraries
+import os
+from copy import deepcopy
+from torchvision.datasets import CIFAR10
+import pickle
+import numpy as np
+import pandas as pd
+
+## Imports for plotting
+import matplotlib.pyplot as plt
+import urllib.request
+from urllib.error import HTTPError
+plt.set_cmap('cividis')
+%matplotlib inline
+from IPython.display import set_matplotlib_formats
+set_matplotlib_formats('svg', 'pdf') # For export
+import matplotlib
+matplotlib.rcParams['lines.linewidth'] = 2.0
+import seaborn as sns
+sns.set()
+
+## tqdm for loading bars
+from tqdm.notebook import tqdm
+
+## PyTorch
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data as data
+import torch.optim as optim
+
+## Torchvision
+import torchvision
+# from torchvision.datasets import CIFAR10
+from torchvision.datasets import CIFAR10
+from torchvision import transforms
+
+# PyTorch Lightning
+try:
+    import pytorch_lightning as pl
+except ModuleNotFoundError: # Google Colab does not have PyTorch Lightning installed by default. Hence, we do it here if necessary
+    !pip install --quiet pytorch-lightning>=1.4
+    import pytorch_lightning as pl
+from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
+
+# Import tensorboard
+%load_ext tensorboard
+
+# Path to the folder where the datasets are/should be downloaded (e.g. CIFAR10)
+DATASET_PATH = "/data"
+# Path to the folder where the pretrained models are saved
+CHECKPOINT_PATH = "/saved_models"
+# In this notebook, we use data loaders with heavier computational processing. It is recommended to use as many
+# workers as possible in a data loader, which corresponds to the number of CPU cores
+NUM_WORKERS = 10
+
+# Setting the seed
+pl.seed_everything(42)
+
+# Ensure that all operations are deterministic on GPU (if used) for reproducibility
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+
+device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+print("Device:", device)
+print("Number of workers:", NUM_WORKERS)
+
+class ContrastiveTransformations(object):
+
+    def __init__(self, base_transforms, n_views=2):
+        self.base_transforms = base_transforms
+        self.n_views = n_views
+
+    def __call__(self, x):
+        return [self.base_transforms(x) for i in range(self.n_views)]
+
+contrast_transforms = transforms.Compose([transforms.RandomHorizontalFlip(),
+                                          transforms.RandomResizedCrop(size=96),
+                                          transforms.RandomApply([
+                                              transforms.ColorJitter(brightness=0.5,
+                                                                     contrast=0.5,
+                                                                     saturation=0.5,
+                                                                     hue=0.1)
+                                          ], p=0.8),
+                                          transforms.RandomGrayscale(p=0.2),
+                                          transforms.GaussianBlur(kernel_size=9),
+                                          transforms.ToTensor(),
+                                          transforms.Normalize(mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010))
+                                          # transforms.Normalize(mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010))
+                                         ])
+
+# Create the datasets
+unlabeled_data = CIFAR10(root=DATASET_PATH, download=True,
+                         transform=ContrastiveTransformations(contrast_transforms, n_views=2))
+train_data_contrast = CIFAR10(root=DATASET_PATH, download=True,
+                              transform=ContrastiveTransformations(contrast_transforms, n_views=2))
+
+# Set all labels in unlabeled_data to -1
+unlabeled_data.targets = [-1 for _ in unlabeled_data.targets]
+
+
+
+class SimCLR(pl.LightningModule):
+
+    def __init__(self, hidden_dim=128, lr=0.0001, temperature=0.1, weight_decay=1e-4, max_epochs=300):
+        super().__init__()
+        self.save_hyperparameters()
+        assert self.hparams.temperature > 0.0, 'The temperature must be a positive float!'
+        # Base model f(.)
+        self.convnet = torchvision.models.resnet50(num_classes=4*hidden_dim)  # Output of last linear layer
+        # The MLP for g(.) consists of Linear->ReLU->Linear
+        self.convnet.fc = nn.Sequential(
+            self.convnet.fc,  # Linear(ResNet output, 4*hidden_dim)
+            nn.ReLU(inplace=True),
+            nn.Linear(4*hidden_dim, hidden_dim)
+        )
+
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters(),
+                                lr=self.hparams.lr,
+                                weight_decay=self.hparams.weight_decay)
+        lr_scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer,
+                                                            T_max=self.hparams.max_epochs,
+                                                            eta_min=self.hparams.lr/50)
+        return [optimizer], [lr_scheduler]
+
+    def info_nce_loss(self, batch, mode='train'):
+        imgs, _ = batch
+        imgs = torch.cat(imgs, dim=0)
+
+        # Encode all images
+        feats = self.convnet(imgs)
+        # Calculate cosine similarity
+        cos_sim = F.cosine_similarity(feats[:,None,:], feats[None,:,:], dim=-1)
+        # Mask out cosine similarity to itself
+        self_mask = torch.eye(cos_sim.shape[0], dtype=torch.bool, device=cos_sim.device)
+        cos_sim.masked_fill_(self_mask, -9e15)
+        # Find positive example -> batch_size//2 away from the original example
+        pos_mask = self_mask.roll(shifts=cos_sim.shape[0]//2, dims=0)
+        # InfoNCE loss
+        cos_sim = cos_sim / self.hparams.temperature
+        nll = -cos_sim[pos_mask] + torch.logsumexp(cos_sim, dim=-1)
+        nll = nll.mean()
+
+        # Logging loss
+        self.log(mode+'_loss', nll)
+        # Get ranking position of positive example
+        comb_sim = torch.cat([cos_sim[pos_mask][:,None],  # First position positive example
+                              cos_sim.masked_fill(pos_mask, -9e15)],
+                             dim=-1)
+        sim_argsort = comb_sim.argsort(dim=-1, descending=True).argmin(dim=-1)
+        # Logging ranking metrics
+        self.log(mode+'_acc_top1', (sim_argsort == 0).float().mean())
+        self.log(mode+'_acc_top5', (sim_argsort < 5).float().mean())
+        self.log(mode+'_acc_mean_pos', 1+sim_argsort.float().mean())
+
+        return nll
+
+    def training_step(self, batch, batch_idx):
+        return self.info_nce_loss(batch, mode='train')
+
+    def validation_step(self, batch, batch_idx):
+        self.info_nce_loss(batch, mode='val')
+        
+def train_simclr(batch_size, max_epochs=300, **kwargs):
+    trainer = pl.Trainer(default_root_dir=os.path.join(CHECKPOINT_PATH, 'SimCLR'),
+                         accelerator="gpu" if str(device).startswith("cuda") else "cpu",
+                         devices=1,
+                         max_epochs=max_epochs,
+                         callbacks=[ModelCheckpoint(save_weights_only=True, mode='max', monitor='val_acc_top5'),
+                                    LearningRateMonitor('epoch')])
+    trainer.logger._default_hp_metric = None # Optional logging argument that we don't need
+
+    # Check whether pretrained model exists. If yes, load it and skip training
+    # pretrained_filename = os.path.join(CHECKPOINT_PATH, 'SimCLR.ckpt')
+    # if os.path.isfile(pretrained_filename):
+    #     print(f'Found pretrained model at {pretrained_filename}, loading...')
+    #     model = SimCLR.load_from_checkpoint(pretrained_filename) # Automatically loads the model with the saved hyperparameters
+    if 1=1:
+        print("New Training begins")
+        train_loader = data.DataLoader(unlabeled_data, batch_size=batch_size, shuffle=True,
+                                        drop_last=True, pin_memory=True, num_workers=NUM_WORKERS)
+        val_loader = data.DataLoader(train_data_contrast, batch_size=batch_size, shuffle=False,
+                                      drop_last=False, pin_memory=True, num_workers=NUM_WORKERS)
+        pl.seed_everything(42) # To be reproducable
+        model = SimCLR(max_epochs=max_epochs, **kwargs)
+        trainer.fit(model, train_loader, val_loader)
+        model = SimCLR.load_from_checkpoint(trainer.checkpoint_callback.best_model_path) # Load best checkpoint after training
+
+    return model
+simclr_model = train_simclr(batch_size=256,
+                            hidden_dim=128,
+                            lr=0.0001,
+                            temperature=0.1,
+                            weight_decay=1e-4,
+                            max_epochs=100)
